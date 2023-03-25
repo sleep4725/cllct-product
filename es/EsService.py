2d23dd3c99e6493d40a5f0ac2ccea6ec6557ef4a
김준현/트렌드플랫폼팀 <junhyeon.kim@gsretail.com>
2023-03-25 16:54:44 +0900

    EsService add

--- es/EsService.py
+++ es/EsService.py
@@ -18,6 +18,87 @@ class EsService:
     es_config = EsArgument.get_config()
     
     @classmethod
+    def is_index_exists(cls, es_client: Elasticsearch, index: str)-> bool:
+        '''
+        :param es_client:
+        :param index:
+        '''
+        is_index_exists :bool= es_client.indices.exists(index=index)
+        print(f"{is_index_exists}")
+        if is_index_exists: return True
+        else: return False
+        
+    @classmethod 
+    def add_index_from_alias(cls, es_client: Elasticsearch, alias: str, index: str):
+        '''
+        :param:
+        :param:
+        :param:
+        :return:
+        '''
+        body = {
+            "actions": [
+                {
+                    "add": {
+                        "index": index,
+                        "alias": alias
+                    }
+                }
+            ]
+        }
+        try:
+                
+            es_client.indices.update_aliases(body=body)
+        except:
+            print(f"alias index[{index}] add fail~!!")
+        else:
+            print(f"alias index[{index}] add success~!!")
+                 
+    @classmethod
+    def delete_index_from_alias(cls, es_client: Elasticsearch, alias: str, index_list: list[str]):
+        '''
+        :param:
+        :param:
+        :param:
+        :return:
+        '''
+        for idx in index_list:
+            body = {
+                "actions": [
+                    {
+                        "remove": {
+                            "index": idx,
+                            "alias": alias
+                        }
+                    }
+                ]
+            }
+            try:
+                
+                es_client.indices.update_aliases(body=body)
+            except:
+                print("")
+            else:
+                print(f"alias index[{idx}] remove success")
+                
+    @classmethod 
+    def exchange_index_of_alias(cls, es_client: Elasticsearch, alias: str, index: str):
+        '''
+        :param es_client:
+        :param alias:
+        :param index:
+        '''
+        # alias 가 ES 에 있는지 확인
+        ## 만약 없다면 index 를 그대로 add
+        ## 만약 있다면
+        ### 기존 index 를 제거 후 새로운 index 를 추가
+        if EsService.is_index_exists(es_client=es_client, index=alias):
+            indices = [idx for idx in es_client.indices.get_alias(alias).keys()]
+            EsService.delete_index_from_alias(es_client=es_client, alias=alias, index_list=indices)
+        else:
+            EsService.add_index_from_alias(es_client=es_client, alias=alias, index=index)
+    
+    @classmethod
     def get_index_name(cls)-> str:
         '''
         :param:
